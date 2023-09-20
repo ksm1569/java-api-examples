5fb92d39283b2a1c1cf8e2ec2422845cbb99cdfc
kimsumin <tnals1564@naver.com>
2023-09-20 13:11:08 +0900

    컬렉션에서 스트림 가져오기 예제

---
+++ src/main/java/com/smsoft/stream_exam/collection/CollectionStreamExam.java
@@ -0,0 +1,21 @@
+package com.smsoft.stream_exam.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class CollectionStreamExam {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        for(int i=1; i<=5; i++) {
+            Product product = new Product(i, "상품" + i, "SMSOFT", (int) (1000*Math.random()));
+            list.add(product);
+        }
+
+        Stream<Product> stream = list.stream();
+        stream.forEach(product -> System.out.println(product));
+    }
+
+}
---
+++ src/main/java/com/smsoft/stream_exam/collection/Product.java
@@ -0,0 +1,32 @@
+package com.smsoft.stream_exam.collection;
+
+public class Product {
+    private int productNo;
+    private String name;
+    private String company;
+    private int price;
+
+    public Product (int productNo, String name, String company, int price) {
+        this.productNo = productNo;
+        this.name = name;
+        this.company = company;
+        this.price = price;
+    }
+
+    public int getProductNo() { return productNo; }
+    public String getName() { return name; }
+    public String getCompany() { return company; }
+    public int getPrice() { return price; }
+
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append("{")
+                .append("productNo:" + productNo + ", ")
+                .append("name:" + name + ", ")
+                .append("company:" + company + ", ")
+                .append("price:" + price)
+                .append("}")
+                .toString();
+    }
+}
