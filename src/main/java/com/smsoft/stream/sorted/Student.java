f05b8af701ed5cff3c6233f240044f10d6c6ab55
ksm1569 <tnals1564@naver.com>
2023-09-21 19:54:36 +0900

    요소정렬

---
+++ src/main/java/com/smsoft/stream/sorted/Student.java
@@ -0,0 +1,19 @@
+package com.smsoft.stream.sorted;
+
+public class Student implements Comparable<Student>{
+    private String name;
+    private int score;
+
+    public Student (String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() { return name; }
+    public int getScore() { return score; }
+
+    @Override
+    public int compareTo(Student o) {
+        return Integer.compare(score, o.score); // 같으면 0, 작으면 -, 크면 +
+    }
+}
