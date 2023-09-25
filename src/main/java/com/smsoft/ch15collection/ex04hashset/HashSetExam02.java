3fe5a666cd8a1e7751d7e41105f030fcece8e399
ksm1569 <tnals1564@naver.com>
2023-09-25 20:09:42 +0900

    1. 자바17로 바꿈 2. hashset 예제

---
+++ src/main/java/com/smsoft/ch15collection/ex04hashset/HashSetExam02.java
@@ -0,0 +1,15 @@
+package com.smsoft.ch15collection.ex04hashset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExam02 {
+    public static void main(String[] args) {
+        Set<Member> set = new HashSet<>();
+
+        set.add(new Member("봉주희", 17));
+        set.add(new Member("봉주희", 17));
+
+        System.out.println("총 객체 수 : " + set.size());
+    }
+}
---
+++ src/main/java/com/smsoft/ch15collection/ex04hashset/Member.java
@@ -0,0 +1,27 @@
+package com.smsoft.ch15collection.ex04hashset;
+
+import java.util.Objects;
+
+public class Member {
+    public String name;
+    public int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Member target) {
+            return target.name.equals(name) && (target.age == age);
+        } else {
+            return false;
+        }
+    }
+}
