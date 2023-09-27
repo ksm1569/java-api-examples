7f1fb8d1ac16b3cf3727571caa23c63946513c05
ksm1569 <tnals1564@naver.com>
2023-09-27 23:30:43 +0900

    Thread join

---
+++ src/main/java/com/smsoft/ch14multithread/ex04threadstatus/JoinExam.java
@@ -0,0 +1,16 @@
+package com.smsoft.ch14multithread.ex04threadstatus;
+
+public class JoinExam {
+    public static void main(String[] args) {
+        SumThread sumThread = new SumThread();
+        sumThread.start();
+        try {
+            sumThread.join();
+        } catch (InterruptedException e) {
+
+        }
+
+        // sumThread가 끝날때까지 기다림
+        System.out.println("1~100 합 : " + sumThread.getSum());
+    }
+}
