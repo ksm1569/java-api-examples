6fd6914c854867b1362edf76dd539d1088a2e398
ksm1569 <tnals1564@naver.com>
2023-10-03 15:32:34 +0900

    다른 생성자 호출방식 (this)

--- src/main/java/com/smsoft/ch6class/ex04overloading/Car01.java
+++ src/main/java/com/smsoft/ch6class/ex04overloading/Car01.java
@@ -1,23 +1,23 @@
 package com.smsoft.ch6class.ex04overloading;
 
-public class Car {
+public class Car01 {
     String company = "삼성자동차";
     String model;
     String color;
     int maxSpeed;
 
-    Car() {}
+    Car01() {}
 
-    Car(String model) {
+    Car01(String model) {
         this.model = model;
     }
 
-    Car(String model, String color) {
+    Car01(String model, String color) {
         this.model = model;
         this.color = color;
     }
 
-    Car(String model, String color, int maxSpeed) {
+    Car01(String model, String color, int maxSpeed) {
         this.model = model;
         this.color = color;
         this.maxSpeed = maxSpeed;
--- src/main/java/com/smsoft/ch6class/ex04overloading/CarExam01.java
+++ src/main/java/com/smsoft/ch6class/ex04overloading/CarExam01.java
@@ -1,23 +1,23 @@
 package com.smsoft.ch6class.ex04overloading;
 
-public class CarExam {
+public class CarExam01 {
     public static void main(String[] args) {
-        Car car1 = new Car();
+        Car01 car1 = new Car01();
         System.out.println("car1.company : " + car1.company);
         System.out.println();
 
-        Car car2 = new Car("자가용");
+        Car01 car2 = new Car01("자가용");
         System.out.println("car2.company : " + car2.company);
         System.out.println("car2.model : " + car2.model);
         System.out.println();
 
-        Car car3 = new Car("자가용", "파란색");
+        Car01 car3 = new Car01("자가용", "파란색");
         System.out.println("car3.company : " + car3.company);
         System.out.println("car3.model : " + car3.model);
         System.out.println("car3.color : " + car3.color);
         System.out.println();
 
-        Car car4 = new Car("택시", "검정", 300);
+        Car01 car4 = new Car01("택시", "검정", 300);
         System.out.println("car4.company : " + car4.company);
         System.out.println("car4.model : " + car4.model);
         System.out.println("car4.color : " + car4.color);
