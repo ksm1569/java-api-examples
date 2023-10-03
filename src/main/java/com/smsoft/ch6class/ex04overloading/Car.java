f447ca56a90c59e936c1a7b8b987f8d7915ab1dd
ksm1569 <tnals1564@naver.com>
2023-10-03 15:20:48 +0900

    생성자 오버로딩

---
+++ src/main/java/com/smsoft/ch6class/ex04overloading/Car.java
@@ -0,0 +1,25 @@
+package com.smsoft.ch6class.ex04overloading;
+
+public class Car {
+    String company = "삼성자동차";
+    String model;
+    String color;
+    int maxSpeed;
+
+    Car() {}
+
+    Car(String model) {
+        this.model = model;
+    }
+
+    Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+}
