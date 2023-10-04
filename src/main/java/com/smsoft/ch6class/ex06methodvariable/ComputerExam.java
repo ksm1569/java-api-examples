82a74a1651a195c873b6e9f6a811a8e9c7df5722
ksm1569 <tnals1564@naver.com>
2023-10-04 23:20:34 +0900

    가변길이 매개변수 메서드

---
+++ src/main/java/com/smsoft/ch6class/ex06methodvariable/ComputerExam.java
@@ -0,0 +1,17 @@
+package com.smsoft.ch6class.ex06methodvariable;
+
+public class ComputerExam {
+    public static void main(String[] args) {
+        Computer myCom = new Computer();
+
+        int result1 = myCom.sum(1,2,3,4);
+        System.out.println("result1 : " + result1);
+
+        int[] values = {1, 2, 3, 4, 5};
+        int result2 = myCom.sum(values);
+        System.out.println("result2 : " + result2);
+
+        int result3 = myCom.sum(new int[] {1, 2, 3, 4, 5});
+        System.out.println("result3 : " + result3);
+    }
+}
