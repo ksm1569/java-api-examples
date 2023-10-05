08708bd22e590bad71f47bda565f097566ee76f0
ksm1569 <tnals1564@naver.com>
2023-10-05 19:35:28 +0900

    method return

---
+++ src/main/java/com/smsoft/ch6class/ex07methodreturn/CarExam.java
@@ -0,0 +1,17 @@
+package com.smsoft.ch6class.ex07methodreturn;
+
+public class CarExam {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        myCar.setGas(10);
+
+        if(myCar.isLeftGas()) {
+            System.out.println("출발합니다.");
+
+            myCar.run();
+        }
+
+        System.out.println("gas를 주입하세요.");
+    }
+}
