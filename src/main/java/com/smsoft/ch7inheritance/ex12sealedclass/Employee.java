65db32256aab00e5cf61d57f392b1494c90c22e0
kimsumin <tnals1564@naver.com>
2023-10-11 17:48:32 +0900

    sealed Class

---
+++ src/main/java/com/smsoft/ch7inheritance/ex12sealedclass/Employee.java
@@ -0,0 +1,8 @@
+package com.smsoft.ch7inheritance.ex12sealedclass;
+
+public final class Employee extends Person{
+    @Override
+    public void work() {
+        System.out.println("제품을 생산합니다.");
+    }
+}
