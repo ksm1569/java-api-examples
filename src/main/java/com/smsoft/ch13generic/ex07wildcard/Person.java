615c3602ae0cb437b262706c94dc4844f40c7cf5
kimsumin <tnals1564@naver.com>
2023-10-13 13:04:36 +0900

    generic wildcard

---
+++ src/main/java/com/smsoft/ch13generic/ex07wildcard/Person.java
@@ -0,0 +1,20 @@
+package com.smsoft.ch13generic.ex07wildcard;
+
+public class Person {
+}
+
+class Worker extends Person {
+
+}
+
+class Student extends Person {
+
+}
+
+class HighStudent extends Student {
+
+}
+
+class MiddleStudent extends Student {
+
+}
